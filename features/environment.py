9ef473e1cb5c1afd79ca62d447feba81d0b2f0cd
ratnasinha012@gmail.com <ratnasinha012@gmail.com>
2025-01-19 16:14:31 -0500

    second commit2

--- features/environment.py
+++ features/environment.py
@@ -1,24 +1,31 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
 from app.application import Application
 
 def browser_init(context):
     """
     :param context: Behave context
     """
+    driver_path = GeckoDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Firefox(service=service)
+
+
     """driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    context.driver = webdriver.Firefox(service=service)
 """
-    # HEADLESS MODE
+    """# HEADLESS MODE
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     service = Service(ChromeDriverManager().install())
     context.driver = webdriver.Chrome(
         options=options,
         service=service
-    )
+    )"""
     context.app = Application(context.driver)
     context.driver.maximize_window()
     context.driver.implicitly_wait(8)
